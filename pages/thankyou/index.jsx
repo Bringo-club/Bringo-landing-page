accfb54bf6417a28ed99c0167f44f7d3f791456e
Imran Baali <ibaali@e1r6p2.1337.ma>
2022-04-05 15:51:48 +0000

    (landing page) add page title and favicon.ico

--- pages/thankyou/index.jsx
+++ pages/thankyou/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Thankyou from "../../components/Thankyou/Thankyou";
 import Footer from "../../components/Footer/Footer";
 
@@ -5,6 +6,11 @@ function T() {
 
 	return (
 		<>
+			<Head>
+				<title>Thank You For Being Awesome!</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="description" content="Bringo lets you make new friends, and do activities together. Starting Soon at UM6P Benguerir" />
+			</Head>
 			<Thankyou />
 			<Footer class="bottom" />
 		</>
