9f94db6f610074c168a6724b72a883417c096a81
Imran Baali <ibaali@e1r5p5.1337.ma>
2022-04-06 14:58:19 +0000

    (landing page) make ui changes

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -14,17 +14,17 @@ function Footer(props) {
 					<path d="M25.9972 34.0754H13.0041C8.82575 34.0754 5.4248 30.6756 5.4248 26.4962V13.5031C5.4248 9.32368 8.82575 5.92383 13.0041 5.92383H25.9972C30.1756 5.92383 33.5765 9.32368 33.5765 13.5031V26.4962C33.5765 30.6756 30.1756 34.0754 25.9972 34.0754ZM13.0041 8.08934C10.0189 8.08934 7.59032 10.518 7.59032 13.5031V26.4962C7.59032 29.4813 10.0189 31.9099 13.0041 31.9099H25.9972C28.9824 31.9099 31.411 29.4813 31.411 26.4962V13.5031C31.411 10.518 28.9824 8.08934 25.9972 8.08934H13.0041Z" fill="#FFFCFC"/>
 					<defs>
 					<radialGradient id="paint0_radial_245_932" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(14.4982 39.5281) scale(48.6149)">
-					<stop stop-color="#FFDD55"/>
-					<stop offset="0.328" stop-color="#FF543F"/>
-					<stop offset="0.348" stop-color="#FC5245"/>
-					<stop offset="0.504" stop-color="#E64771"/>
-					<stop offset="0.643" stop-color="#D53E91"/>
-					<stop offset="0.761" stop-color="#CC39A4"/>
-					<stop offset="0.841" stop-color="#C837AB"/>
+					<stop stopColor="#FFDD55"/>
+					<stop offset="0.328" stopColor="#FF543F"/>
+					<stop offset="0.348" stopColor="#FC5245"/>
+					<stop offset="0.504" stopColor="#E64771"/>
+					<stop offset="0.643" stopColor="#D53E91"/>
+					<stop offset="0.761" stopColor="#CC39A4"/>
+					<stop offset="0.841" stopColor="#C837AB"/>
 					</radialGradient>
 					<radialGradient id="paint1_radial_245_932" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(6.27569 0.0130882) scale(32.2804 21.5084)">
-					<stop stop-color="#4168C9"/>
-					<stop offset="0.999" stop-color="#4168C9" stop-opacity="0"/>
+					<stop stopColor="#4168C9"/>
+					<stop offset="0.999" stopColor="#4168C9" stopOpacity="0"/>
 					</radialGradient>
 					</defs>
 				</svg>
